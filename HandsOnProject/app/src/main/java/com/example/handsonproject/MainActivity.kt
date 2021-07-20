927736f7d2d9a105309b41677742a0057af767f6
shovian <hideyoshishofian@gmail.com>
2021-07-20 13:45:37 +0700

    Add button functionality

--- HandsOnProject/app/src/main/java/com/example/handsonproject/MainActivity.kt
+++ HandsOnProject/app/src/main/java/com/example/handsonproject/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.handsonproject
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
+    val btn : Button = findViewById(R.id.button1) as Button
+    val input :
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
