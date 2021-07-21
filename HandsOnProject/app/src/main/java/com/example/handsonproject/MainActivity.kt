7a251766ce3c1a60368d1f2cb8ab5b9efaa7043c
shovian <hideyoshishofian@gmail.com>
2021-07-21 13:46:45 +0700

    Added functional to button, but still has some issues unknown

--- HandsOnProject/app/src/main/java/com/example/handsonproject/MainActivity.kt
+++ HandsOnProject/app/src/main/java/com/example/handsonproject/MainActivity.kt
@@ -3,17 +3,20 @@ package com.example.handsonproject
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 
 
 class MainActivity : AppCompatActivity() {
-    val btn : Button = findViewById(R.id.button1) as Button
-    val input :
+    val btn : Button = findViewById(R.id.button23) as Button
+    val input : EditText = findViewById(R.id.editText1) as EditText
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btn.setOnClickListener {
+            input.setText("a")
+        }
     }
 }
